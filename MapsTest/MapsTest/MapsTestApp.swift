1b3cd806f77d5f233b3d91cc0b4c69bddf787144
mkuznetsov-dialpro <mvk0402@gmail.com>
2024-08-05 22:00:51 +0300

    Initial commit

---
+++ MapsTest/MapsTest/MapsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsTestApp.swift
+//  MapsTest
+//
+//  Created by Максим Кузнецов on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
